0931c5bbb3447de18abcf4d2a89a72d70e3adc4a
45kiru <pokhrel.kiran14@gmail.com>
2023-09-29 10:56:10 +0545

    feat: add student class
    introduce new class student which matches the fields of csv data

---
+++ Student.java
@@ -0,0 +1,21 @@
+class Student {
+    private String unitName;
+    private String lastName;
+    private String firstName;
+    private String studentId;
+    private double assignment1;
+    private double assignment2;
+    private double assignment3;
+    private double totalMarks;
+
+    public Student(String unitName, String lastName, String firstName, String studentId, double assignment1, double assignment2, double assignment3) {
+        this.unitName = unitName;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.studentId = studentId;
+        this.assignment1 = assignment1;
+        this.assignment2 = assignment2;
+        this.assignment3 = assignment3;
+    }
+
+}
